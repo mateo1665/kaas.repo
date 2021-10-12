2ec5d9ddeb7134a97aac87bd3a93da7ab74cdc2a
mateo1665 <89844584+mateo1665@users.noreply.github.com>
2021-10-12 11:32:14 +0200

    Add files via upload

---
+++ else statement.py
@@ -0,0 +1,12 @@
+saldo = 10
+if saldo  > 12:
+    print('je kunt het a product kopen')
+else:
+    print('saldo is te laag')
+
+saldo = 78
+if saldo > 78:
+    print('je kunt het product kopen')
+else:
+    print('je hebt genoeg saldo om het product te betalen')
+
